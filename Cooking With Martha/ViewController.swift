c8693e6f07f720bcfea18b114518cacafa06b4e8
Matthew Lock <matthew_lock20@hotmail.com>
2022-05-07 18:19:29 +0100

    Initial Commit

---
+++ Cooking With Martha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cooking With Martha
+//
+//  Created by Matthew Lock on 07/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cooking With MarthaUITests/Cooking_With_MarthaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Cooking_With_MarthaUITestsLaunchTests.swift
+//  Cooking With MarthaUITests
+//
+//  Created by Matthew Lock on 07/05/2022.
+//
+
+import XCTest
+
+class Cooking_With_MarthaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
