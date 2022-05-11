d489ab243dd619976f23ac66ab07e9c0859a986d
Matthew Lock <matthew_lock20@hotmail.com>
2022-05-11 21:17:34 +0100

    Added DataManager, not yet operational, still need to reconfigure structs

--- Cooking With Martha/Item.swift
+++ Cooking With Martha/Item.swift
@@ -2,13 +2,14 @@
 //  Recipe.swift
 //  Cooking With Martha
 //
-//  Created by Matthew Lock on 11/05/2022.
+//  Created by Matthew Lock on 08/05/2022.
 //
 
 import Foundation
+import CardSlider
 
-struct Recipe: Decodable {
-    var image: String
+struct Item: CardSliderItem {
+    var image: UIImage
     var rating: Int?
     var title: String
     var subtitle: String?
