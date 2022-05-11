d79084a3c330b65464388d70bda336a44aacfed2
Matthew Lock <matthew_lock20@hotmail.com>
2022-05-11 19:18:10 +0100

    Added hardcoded card to test functionality

--- Cooking With Martha/ViewController.swift
+++ Cooking With Martha/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     
     var data = [Recipe]()
 
+    let imageURL = URL(string: "https://www.themealdb.com/images/media/meals/58oia61564916529.jpg")!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(header)
@@ -20,8 +22,7 @@ class ViewController: UIViewController {
         view.addSubview(deleteButton)
         view.addSubview(category)
         view.addSubview(recipeButton)
-        data.append(Recipe(image: UIImage(systemName: "bookmark")!, rating: nil, title: "bookmarks", subtitle: "ghet excited about bookmarks", description: "You can add bookmarks"))
-        data.append(Recipe(image: UIImage(systemName: "bookmark")!, rating: nil, title: "bookmarks", subtitle: "ghet excited about bookmarks", description: "You can add bookmarks"))
+        data.append(Recipe(image: try! UIImage(withContentsOfUrl: imageURL)!, rating: 1, title: "Corba", subtitle: "Turkish", description: "Pick through your lentils for any foreign debris, rinse them 2 or 3 times, drain, and set aside.  Fair warning, this will probably turn your lentils into a solid block that you’ll have to break up later\r\nIn a large pot over medium-high heat, sauté the olive oil and the onion with a pinch of salt for about 3 minutes, then add the carrots and cook for another 3 minutes.\r\nAdd the tomato paste and stir it around for around 1 minute. Now add the cumin, paprika, mint, thyme, black pepper, and red pepper as quickly as you can and stir for 10 seconds to bloom the spices. Congratulate yourself on how amazing your house now smells.\r\nImmediately add the lentils, water, broth, and salt. Bring the soup to a (gentle) boil.\r\nAfter it has come to a boil, reduce heat to medium-low, cover the pot halfway, and cook for 15-20 minutes or until the lentils have fallen apart and the carrots are completely cooked.\r\nAfter the soup has cooked and the lentils are tender, blend the soup either in a blender or simply use a hand blender to reach the consistency you desire. Taste for seasoning and add more salt if necessary.\r\nServe with crushed-up crackers, torn up bread, or something else to add some extra thickness.  You could also use a traditional thickener (like cornstarch or flour), but I prefer to add crackers for some texture and saltiness.  Makes great leftovers, stays good in the fridge for about a week."))
     }
 
     let header: UILabel = {
@@ -103,4 +104,29 @@ extension ViewController: CardSliderDataSource {
     }
     
     
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+extension UIImage {
+
+    convenience init?(withContentsOfUrl url: URL) throws {
+        let imageData = try Data(contentsOf: url)
+    
+        self.init(data: imageData)
+    }
+
 }
